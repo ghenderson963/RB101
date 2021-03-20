89870d7d1d2694a27f8be66264609f9cf388ae7d
Gary Henderson <ghenderson963@gmail.com>
2021-03-20 15:51:31 -0700

    Finished refactor of calculatorv2

--- lesson_2/calculatorv1.rb
+++ lesson_2/calculatorv1.rb
@@ -4,35 +4,41 @@ require 'pry'
 # - asks for the type of operation to perform: add, subtract, multiply or divide
 # - diplays the result
 play_again = 'y'
+answer = 'n'
+num1 = nil
+num2 = nil
 while play_again == 'y'
 
-    begin
-      puts "Enter a the first number."
-      num1 = gets.chomp
-    end until num1 == num1.to_i.to_s
-    
-    begin
-      puts "Enter the second number."
-      num2 = gets.chomp
-    end until num2 == num2.to_i.to_s
+  loop do
+    puts "Enter the first number."
+    num1 = gets.chomp
+    break if num1 == num1.to_i.to_s
+  end
 
-  begin
+  loop do
+    puts "Enter the second number."
+    num2 = gets.chomp
+    break if num2 == num2.to_i.to_s
+  end
+
+  loop do
     puts "Would you like to (a)dd, (s)ubtract, (m)ultiply or (d)ivide?"
     answer = gets.chomp.downcase
-  end until answer == 'a' || answer == 's' || answer == 'm' || answer == 'd'
-  
+    break if answer == 'a' || answer == 's' || answer == 'm' || answer == 'd'
+  end
+
   def add(num1, num2)
     num1.to_i + num2.to_i
   end
-  
+
   def subtract(num1, num2)
     num1.to_i - num2.to_i
   end
-  
+
   def multiply(num1, num2)
     num1.to_i * num2.to_i
   end
-  
+
   def divide(num1, num2)
     if num2.to_i == 0
       "Can't divide by 0"
@@ -40,7 +46,7 @@ while play_again == 'y'
       num1.to_f / num2.to_f
     end
   end
-  
+
   case answer
   when 'a'
     puts "#{num1} + #{num2} = #{add(num1, num2)}"
@@ -50,14 +56,13 @@ while play_again == 'y'
     puts "#{num1} * #{num2} = #{multiply(num1, num2)}"
   else
     if num2.to_i == 0
-      puts "Can't divide by zero." 
+      puts "Can't divide by zero."
     else
-      puts "#{num1} / #{num2} = #{divide(num1,num2)}"
+      puts "#{num1} / #{num2} = #{divide(num1, num2)}"
     end
   end
 
   puts "Calculate more numbers? (y)es or (n)o?"
   play_again = gets.chomp.downcase
-  
+
 end
- 
--- lesson_2/calculatorv2.rb
+++ lesson_2/calculatorv2.rb
@@ -1,24 +1,100 @@
-
-
-puts "Welcome to Calculator!"
-
-puts "What's the first number?"
-num1 = gets.chomp
-
-puts "What's the second number?"
-num2 = gets.chomp
-
-puts "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide"
-operator = gets.chomp
-
-if operator == '1'
-  result = num1.to_i + num2.to_i
-elsif operator == '2'
-  result = num1.to_i - num2.to_i
-elsif operator == '3'
-  result = num1.to_i * num2.to_i
-else
-  result = num1.to_f / num2.to_f
+def prompt(message)
+  puts "=> #{message}"
 end
 
-puts "The result is #{result}"
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt "Welcome to Calculator! Enter your name:"
+
+name = ''
+loop do
+  name = gets.chomp
+
+  if name.empty?
+    prompt "Make sure you use a valid name."
+  else
+    break
+  end
+end
+
+loop do
+  num1 = ''
+  loop do
+    prompt "What's the first number?"
+    num1 = gets.chomp
+
+    if valid_number?(num1)
+      break
+    else
+      prompt("hmm something is wrong.  That isn't a valid number.")
+    end
+  end
+
+  num2 = ''
+  loop do
+    prompt "What's the second number?"
+    num2 = gets.chomp
+
+    if valid_number?(num2)
+      break
+    else
+      prompt("hmmm something is wrong.  Please enter a valid number.")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+     => 1) add
+     => 2) subtract
+     => 3) multiply
+     => 4) divide
+  MSG
+
+  prompt operator_prompt
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt "Must chosse 1, 2 ,3, or 4"
+    end
+  end
+
+  prompt "#{operation_to_message(operator)} the two numbers..."
+
+  result = case operator
+           when '1'
+             num1.to_i + num2.to_i
+           when '2'
+             num1.to_i - num2.to_i
+           when '3'
+             num1.to_i * num2.to_i
+           when '4'
+             num1.to_f / num2.to_f
+           end
+
+  prompt "The result is #{result}"
+
+  prompt "Do you want to perform another calculation (Y to calculate again)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thank you for using calculator."
