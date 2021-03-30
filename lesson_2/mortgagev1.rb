e0f206919e21ed3a3c684a763cae7533fdafb98d
Gary Henderson <ghenderson963@gmail.com>
2021-03-29 20:38:00 -0700

    Refactored mortgagev1.rb

--- lesson_2/mortgagev1.rb
+++ lesson_2/mortgagev1.rb
@@ -15,15 +15,15 @@ def prompt(str)
 end
 
 def valid_float?(amount)
-  amount.to_s.to_f == amount
+  amount.to_s.to_f == amount && amount.to_i > 0
 end
 
 def valid_integer?(num)
-  num.to_i.to_s == num
+  num.to_i.to_s == num && num.to_i > 0
 end
 
 def valid_percentage_rate?(rate)
-  rate.to_s.to_f == rate
+  rate.to_s.to_f == rate && rate.to_f > 0
 end
 
 def format_apr(decimal)
@@ -39,51 +39,43 @@ def monthly_payment(monthly_percentage_rate, loan_duration_months, loan_amount)
   (1 + monthly_percentage_rate)**(-loan_duration_months)))
 end
 
+system "clear"
 prompt('Hi welcome to the mortgage calculator!')
 
 loan_amount = 0
 loop do
   prompt('What is the loan amount in dollars?')
   loan_amount = gets.chomp.to_f
-  if valid_float?(loan_amount)
-    break
-  else
-    prompt("#{loan_amount} doesn't appear to be an valid loan amount.")
-  end
+  break if valid_float?(loan_amount)
+  prompt("#{format('%.0f',loan_amount)} doesn't appear to be an valid loan amount.")
 end
 
 formatted_apr = ''
 loop do
-  prompt('What is the Annual Percentage Rate
-  expressed as a decimal percentage?')
+  prompt("What is the Annual Percentage " \
+          "Rate expressed as a decimal percentage?")
   prompt('For example, 3.75% or 2.50%')
   annual_percentage_rate = gets.chomp
   formatted_apr = format_apr(annual_percentage_rate)
-  if valid_percentage_rate?(formatted_apr)
-    break
-  else
-    prompt("That doesn't appear to be a valid annual percentage rate.")
-  end
+  break if valid_percentage_rate?(formatted_apr)
+  prompt("#{annual_percentage_rate} doesn't appear to be a valid annual percentage rate.")
 end
 
 loan_duration = ''
 loop do
   prompt('What is the loan duration in years.')
   loan_duration = gets.chomp
-
-  if valid_integer?(loan_duration)
-    break
-  else
-    prompt("That doesn't look like a valid loan duration in months.")
-  end
+  break if valid_integer?(loan_duration)
+  prompt("That doesn't look like a valid loan duration in years.")
 end
 
 monthly_percentage_rate = formatted_apr.to_f / 12
 loan_duration_months = loan_duration.to_i * 12
 
-puts "The monthly interest rate is #{monthly_percentage_rate * 100}%."
-puts "The loan duration is #{loan_duration_months} months."
-puts "The monthly payment is $#{format('%.2f',
+system "clear"
+prompt "The monthly interest rate is #{monthly_percentage_rate * 100}%."
+prompt "The loan duration is #{loan_duration_months} months."
+prompt "The monthly payment is $#{format('%.2f',
 monthly_payment(monthly_percentage_rate,
                 loan_duration_months,
                 loan_amount.to_f))}."
