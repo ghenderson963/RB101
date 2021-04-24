1a0f7e07c9cdf9dc38d7ab9231e66cf26287eeab
Gary Henderson <ghenderson963@gmail.com>
2021-04-24 15:37:08 -0700

    Manual merge of easy1.rb

--- small_problems/easy1.rb
+++ small_problems/easy1.rb
@@ -1,3 +1,60 @@
+# 1. Write a method that takes 2 arguements a string and a positive integer and prints the string as many times 
+# as the integer indicates
+# - takes 2 arguements string and positive integer
+# - prints the strings as many times as the integer
+# -- define the method which takes 2 arguements str, int
+# -- iterate the number of the given integer
+
+def repeat(str, int)
+  int.times { puts str }
+end
+
+repeat('Hello', 3)
+
+# 2. Write a method that takes one interger argument, which may be positive, negative, or zero.  The method returns true
+# if the number's absolute value is odd.  You may assume that the argument is a valid integer value.
+# input - integer positive negative or zero
+# output - bolean true or false
+# define the method
+# take the absolute value of the integer
+# evaluate if the integer is odd or even
+# return true if the number is odd
+#
+#def is_odd?(num)
+  #num % 2 != 0
+#end
+
+def is_odd?(num)
+  num % 2 == 1
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
+
+# Write a method that takes one arguement a positive integer and returns a list of the digits in the number
+# Input - positive integer
+# output - array of digits
+# - define the method
+# - initialize an array for the results
+# - turn the number into a string
+# - split the string up into substrings in an array
+# - iterate through the array and change each string into an integer
+def digit_list(num)
+  num.to_s.chars.map(&:to_i)
+end
+
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
+
+
+
 # write a method that counts the number of occurances of 
 # element of a given array.
 # case of the elements matters
