15a0c7193ca797ca0fd0e3789942f236757da3f6
Gary <ghenderson963@gmail.com>
2021-05-15 18:42:07 -0700

    working on computer turn logic in test.rb

--- lesson_3/hard1.rb
+++ lesson_3/hard1.rb
@@ -1,32 +1,61 @@
-# 1.
+# 1. What will happen to the greating variable in the code below.
+if false
+    greeting = "hello world"
+  end
+  
+  greeting
 
+# PROBLEM:
 
-# 2.
+# Given a string, write a method `palindrome_substrings` which returns
+# all the substrings from a given string which are palindromes. Consider
+# palindrome words case sensitive.
 
+# Test cases:
 
+# palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
+# palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
+# palindrome_substrings("palindrome") == []
+# palindrome_substrings("") == []
 
-# 3.
+# input: string
+# output: array of strings
+# 1. What is a substring.  
+# 2. empty string returns empty array
+# 3. leave any caps the same only add caps to the full palindrom
+# 4. assume all inputs will be strings
+# 5. what does it mean to treat palindrome words case sensitive
 
+# rules:
+# 1. return only strings that are palindromes
+# 2. empty string returns empty string
+# 3. palidromes must have the same case.
 
+halo - ha, hal, halo, al, alo, lo 
+slice[0,2]
+slice[0,3]
+slice[0,4] 
+i and j when j == string.length
+slice[1,2]
+slice[1,3]   j >= string.length
+slice[2,3]   j >= string.length
 
-# 4. Fix the code below so that it returns a false condition and handles the case were more or fewer than 4 components
-# to the IP address or given.
-
-def is_an_ip_number?(num)
-  num.to_i > 0 && num.to_i <= 255
-end
-
-def dot_separated_ip_address?(input_string)
-  dot_separated_words = input_string.split(".")
-  return false unless dot_separated_words.count == 4
-
-  number_check = dot_separated_words.map do |word|
-  is_an_ip_number?(word)
+2 loops 1 inner and 1 outer
+i = 0
+j = 2
+result = []
+loop do
+    
+  loop do
+    temp = ''
+    break if j >= str.length
+    temp = str[i,j]
+    result << if is_palindrome?(temp)
+    i += 1
   end
 
-  !number_check.include?(false)
-end
+  
 
-puts dot_separated_ip_address?('233.111.234.123')
-
-
+loop do
+  loop do
+    
--- lesson_6/test.rb
+++ lesson_6/test.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Winning selections
 # across
 # 0,1,2
@@ -12,12 +14,99 @@
 # 2,4,6
 
 # Computer turn
-# 1. check if any of the winning plays is close by seeing if 2 of the 3 squares are played.
-#    a. break all the winning triples up into sub arrays.
-#       i. 
+# 1. check if any of the winning plays is close by seeing if 2 of 
+#    the 3 squares are played.
+#    a. break all the win, ning triples up into sub arrays.
+#       i. arr[0,3]
+#          arr[3,3]
+#          arr[6,3]
+#          [[arr[0],arr[3],arr[6]]
+#          [[arr[1],arr[4],arr[7]]
+#          [[arr[2],arr[5],arr[8]]
+#          [[arr[0],arr[4],arr[8]]
+#          [[arr[2],[arr[4],arr[6]]
+current_player = 'player'
+
+def place_piece(board,current_player,square)
+  if current_player == 'player'
+    board[square] = 'X'
+  else
+    board[square] = 'O'
+  end
+end
+
+def check_across(board, piece)
+  counter = 0
+  loop do
+    # binding.pry
+    if board[counter,3].count(piece) == 2
+      board[counter,3].each do |square|
+        #binding.pry
+        return square unless square == piece
+      end
+        break
+    else
+      counter += 3
+      break if counter > 8
+      next
+    end
+    return false
+    end
+  end
+
+def check_down(board,piece)
+  counter = 0
+  loop do
+    #binding.pry
+    if [board[counter],board[counter + 3], board[counter + 6]].count(piece) == 2
+      binding.pry
+      [board[counter],board[counter + 3], board[counter + 6]].each do |square|
+        return square unless square == piece
+      end
+    else
+      counter += 1
+      break if counter > 2
+    end
+
+  end
+  return false
+end
+
+def check_diagonal(board, piece)
+  if [board[0],board[4], board[8]].count == 2
+    [board[0],board[4], board[8]].each do |square|
+      return square unless square == piece 
+    end
+  elsif [board[2],board[4],board[6]].count == 2
+    [board[2],board[4],board[6]].each do |square|
+      return square unless square == piece
+    end
+  else
+    return false
+  end
+    #          [[arr[2],[arr[4],arr[6]]
+end
+
+def computer_turn(board,piece)
+  current_player = 'computer'
+  place_piece(board,current_player,check_across(board,piece)) if check_across(board,piece)
+  place_piece(board,current_player,check_down(board, piece)) if check_down(board,piece)
+  place_piece(board,current_player,check_diagonal(board, piece)) if check_diagonal(board, piece)
+
+end
+
+piece = 'X'
+current_player = 'computer'
+board = [0,'X',2,3,'X',5,6,7,8]
+computer_turn(board, piece)
+p board
+# p check_across(board,piece)
+# p check_down(board, piece)
+# p check_diagonal(board, piece)
+
 #    b. return all the sub arrays that have 2 plays.
 # 2. if 2 of the 3 are played play the first open square.
 # 3. 
 
-if board[i] == 'X' && board[i + 1] == 'X'
-  next unless square_free?(board, board[i + 2 ])
+#if board[i] == 'X' && board[i + 1] == 'X'
+#  next unless square_free?(board, board[i + 2 ])
