ab645eaacc74739c22d27c76a899a29c87a1e759
Gary Henderson <ghenderson963@gmail.com>
2021-05-22 23:56:43 -0700

    Fixed several bugs tttv1.rb

--- lesson_6/tttv1.rb
+++ lesson_6/tttv1.rb
@@ -9,7 +9,7 @@ def display_table(board)
 
   puts "                             "
   puts "          |       |          "
-  puts "      #{board[0]}   |   #{board[1]}   |   #{board[2]}          "
+  puts "      #{board[0]}   |   #{board[1]}   |  #{board[2]}         "
   puts "   _______|_______|_______   "
   puts "          |       |          "
   puts "      #{board[3]}   |   #{board[4]}   |  #{board[5]}          "
@@ -48,6 +48,10 @@ def switch_player(player)
   return 'Player' if player == 'Computer'
 end
 
+def list_of_empty_squares(board)
+  board.select { |num| num != 'O' && num != 'X' }
+end
+
 def square_free?(board, square)
   board[square] != 'X' && board[square] != 'O'
 end
@@ -74,8 +78,8 @@ end
 def check_down(board)
   counter = 0
   loop do
-
-    if [board[counter], board[counter + 3], board[counter + 6]].count('X') == 2 || [board[counter], board[counter + 3], board[counter + 6]].count('O') == 2
+    if [board[counter], board[counter + 3], board[counter + 6]].count('X') == 2 \
+      || [board[counter], board[counter + 3], board[counter + 6]].count('O') == 2
       [board[counter],board[counter + 3], board[counter + 6]].each do |square|
         return square unless square == 'X' || square == 'O'
       end
@@ -121,56 +125,62 @@ def check_last_square(board)
   return selection[0] if selection[0]
 end
 
-def computer_turn(board, current_player_symbol)
+def computer_turn(board)
   if check_last_square(board)
     check_last_square(board) - 1
   elsif check_across(board)
     check_across(board) - 1
   elsif check_down(board)
-    check_down(board) -1
+    check_down(board) - 1
   elsif check_diagonal(board)
-    check_diagonal(board) -1
+    check_diagonal(board) - 1
   elsif check_middle(board)
-    check_middle(board) -1
+    check_middle(board) - 1
   else
-    random_square(board) -1
+    random_square(board) - 1
   end
 end
 
-def player_turn(player_symbol)
+def player_turn(board)
   player_input = ''
-    loop do 
-      prompt "Select a square numbered 1-9."
-      player_input = gets.chomp.to_i - 1
-      return player_input if (1..9).include?(player_input)
-      prompt "The number needs to be between 1 - 9!"
-      puts ''
-    end
+  loop do
+    prompt "Select a square numbered #{list_of_empty_squares(board).join(', ')}."
+    player_input = gets.chomp.to_i - 1
+    return player_input if (0..9).include?(player_input)
+    prompt "The number needs to be between 1 - 9!"
+    puts ''
+  end
     
 end
 
-def current_player_turn(board,current_player, player_symbol)
-  current_player == 'Player' ? player_turn(board, player_symbol) : computer_turn(board, player_symbol)
+def current_player_turn(board, current_player, player_symbol)
+  current_player == 'Player' ? player_turn(board) : computer_turn(board)
 end
 
 def winner?(board)
 
   counter = 0
   loop do 
-    return true if board[counter,3].count('X') == 3 || board[counter,3].count('O') == 3
+    return true if board[counter, 3].count('X') == 3 || board[counter, 3].count('O') == 3
     break if counter > 7
     counter += 3
   end
 
   counter = 0
   loop do
-    return true if [board[counter], board[counter + 3], board[counter + 6]].count('X') == 3 || [board[counter], board[counter + 3], board[counter + 6]].count('O') == 3
-    break if counter > 3
-    counter += 1
-  end 
+    if [board[counter], board[counter + 3], board[counter + 6]].count('X') == 3 ||
+       [board[counter], board[counter + 3], board[counter + 6]].count('O') == 3
+      return true
+    else
+      break if counter > 3
+      counter += 1
+    end
+  end
 
-  return true if [board[0], board[4], board[8]].count('X') == 3 || [board[0], board[4], board[8]].count('O') == 3
-  return true if [board[2], board[4], board[6]].count('X') == 3 || [board[2], board[4], board[6]].count('O') == 3
+  return true if [board[0], board[4], board[8]].count('X') == 3 ||
+                 [board[0], board[4], board[8]].count('O') == 3
+  return true if [board[2], board[4], board[6]].count('X') == 3 ||
+                 [board[2], board[4], board[6]].count('O') == 3
 end
 
 def board_full?(board)
@@ -180,50 +190,63 @@ end
 loop do
   system("clear")
   prompt "Welcome to Tic Tac Toe!"
+  current_player = pick_first_player
+  player_symbol = choose_symbol
+  computer_symbol = computer_symbol(player_symbol)
+  current_player_symbol = if current_player == 'Player'
+                            player_symbol
+                          else
+                            computer_symbol
+                          end
+
   prompt "Choosing who goes first...."
   5.times do
     sleep 0.25
     print '.'
   end
   puts '.'
-  current_player = pick_first_player
   prompt "#{current_player} goes first!"
-  
-  player_symbol = choose_symbol
-  computer_symbol = computer_symbol(player_symbol)
-  current_player_symbol = current_player == 'Player' ? player_symbol : computer_symbol
-  
+  sleep 2
+
   board = initialize_board
   display_table(board)
   square = 10
   loop do
-
-    loop do 
-      square = current_player_turn(board,current_player, current_player_symbol)
+    loop do
+      square = current_player_turn(board, current_player, current_player_symbol)
       if square_free?(board, square)
         place_piece(board, square, current_player_symbol)
         break
+      elsif (1..9).include?(square)
+        prompt "That square is taken. Please choose another."
       else
-        (1..9).include?(square) ? (prompt "That square is taken.  Please choose another.") : 
-        (prompt "Please enter a number (1-9).")
+        prompt "Please enter a number (1-9)."
       end
     end
 
-    break if board_full?(board)
-    break if winner?(board)
+    break if board_full?(board) || winner?(board)
     sleep 1
     display_table(board)
     prompt "#{current_player} chooses to play square #{square + 1}."
     sleep 1
     current_player = switch_player(current_player)
-    current_player_symbol = current_player == 'Player' ? player_symbol : computer_symbol
+    current_player_symbol = if current_player == 'Player'
+                              player_symbol
+                            else
+                              computer_symbol
+                            end
     sleep 1
   end
+
   display_table(board)
-  
   puts ''
-  board_full?(board) ? (prompt "Looks like it is a tie!") : (prompt "#{current_player} wins!")
-  
+
+  if board_full?(board)
+    prompt "Looks like it is a tie!"
+  else
+    prompt "#{current_player} wins!"
+  end
+
   puts ''
   prompt "Want to play again? (N)o or (Y)es."
   play_again = gets.chomp.upcase
