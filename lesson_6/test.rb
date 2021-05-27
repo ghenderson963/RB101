621379d2cad4442492771e88e5f7db5606551502
Gary Henderson <ghenderson963@gmail.com>
2021-05-26 22:03:50 -0700

    started tttv2.rb messing around with computer's turn.

--- lesson_6/test.rb
+++ lesson_6/test.rb
@@ -26,252 +26,147 @@ require 'pry'
 #          [[arr[0],arr[4],arr[8]]
 #          [[arr[2],[arr[4],arr[6]]
 
-def prompt(message)
-  puts "==> #{message}"
-end
+WINNING_LINES = [[0,1,2],[3,4,5],[6,7,8],
+                 [0,3,6],[1,4,7],[2,5,8],
+                 [0,4,8],[2,4,6]]
 
-def display_table(board)
-  system("clear")
-
-  puts "                             "
-  puts "          |       |          "
-  puts "      #{board[0]}   |   #{board[1]}   |   #{board[2]}          "
-  puts "   _______|_______|_______   "
-  puts "          |       |          "
-  puts "      #{board[3]}   |   #{board[4]}   |  #{board[5]}          "
-  puts "   _______|_______|_______   "
-  puts "          |       |          "
-  puts "      #{board[6]}   |   #{board[7]}   |  #{board[8]}          "
-  puts "          |       |          "
-  puts "                             "
-end
-
-def pick_first_player
-  ['Player','Computer'].sample
-end
-
-def choose_symbol
-  loop do
-    puts ''
-    prompt "Would you like to be Xs or Os?"
-    player_piece = gets.chomp
-    return player_piece if ['X','O'].include?(player_piece)
-    prompt "Please choose an X or an O."
-  end
-end
-
-def set_computer_symbol(player_symbol)
-  player_symbol == 'X' ? computer_piece = 'O' : computer_piece = 'X'
-end
-
-def initialize_board
-  new_board = []
-  (1..9).map { |num| new_board[num] = num }
-end
-
-def switch_player(player)
-  return 'Computer' if player == 'Player'
-  return 'Player' if player == 'Computer'
-end
 
 def square_free?(board, square)
   board[square] != 'X' && board[square] != 'O'
 end
 
-def place_piece(board,current_player,square, current_player_symbol)
-
-  if current_player == 'player'
-    board[square] = current_player_symbol
-  else
-    board[square] = current_player_symbol
-  end
-  board
+def place_piece(board, square, current_player_symbol)
+  board[square] = current_player_symbol
 end
 
-def check_across(board)
-  counter = 0
-  loop do
+def change_symbol(player_symbol)
+  player_symbol == 'X' ? 'O' : 'X'
+end
 
-    if board[counter,3].count('X') == 2 || board[counter,3].count('O') == 2
-      board[counter,3].each do |square|
-        return square unless square == 'O' || square == 'X'
+def list_of_empty_squares(board)
+  board.select { |num| num != 'O' && num != 'X' }
+end
+
+def find_potential_plays(board, symbol)
+  squares = []
+  WINNING_LINES.each_with_index do |line, idx|
+    squares << [line.select { |num| board[num] == symbol}.count, idx]
+  end
+
+  squares.select { |indx_count| indx_count[0] == 2 }.flatten
+  return false if squares.empty?
+end
+
+def choose_which_play(board, symbol)
+      #square = []
+      squares_to_play = find_potential_plays(board, symbol)
+
+      WINNING_LINES[squares_to_play[1]].each do |num|
+
+        square = if board[num] != symbol && square_free?(board, num)
+                    board[num]
+                 end
       end
-    else
-      counter += 3
-      break if counter > 8
-    end
-    return false
-  end
-
-  return false
-  end
-
-def check_down(board)
-  counter = 0
-  loop do
-
-    if [board[counter], board[counter + 3], board[counter + 6]].count('X') == 2 || [board[counter], board[counter + 3], board[counter + 6]].count('O') == 2
-      [board[counter],board[counter + 3], board[counter + 6]].each do |square|
-        return square unless square == 'X' || square == 'O'
-      end
-      return false
-    else
-      counter += 1
-      break if counter > 2
-    end
-  end
-
-  return false
+    return square[0]
 end
 
-def check_diagonal(board)
-
-  if [board[0], board[4], board[8]].count('X') == 2 || [board[0], board[4], board[8]].count('O') == 2
-    [board[0], board[4], board[8]].each do |square|
-      return square unless square == 'X' || square == 'O'
-    end
-  elsif [board[2],board[4],board[6]].count('X') == 2 || [board[2],board[4],board[6]].count('O') == 2
-    [board[2],board[4],board[6]].each do |square|
-      return square unless square == 'X' || square == 'O'
-    end
-  end
-  return false
-end
-
-def check_middle(board)
-  return 5 if board[4] == 5
-end
-
-def random_square(board)
-  loop do
-    square = board.sample 
-    return square if (0..8).include?(square)
-  end
-end
-
-def check_last_square(board)
-  selection = []
-  if board.count('X') + board.count('O') == 8
-    selection = board.select { |square| square != 'X' && square != 'O' }
-  end
-  return selection[0] if selection[0]
-end
-
-def computer_turn(board, current_player_symbol)
-  current_player = 'computer'
-
-  if check_last_square(board)
-    check_last_square(board) - 1
-  elsif check_across(board)
-    check_across(board) - 1
-  elsif check_down(board)
-    check_down(board) -1
-  elsif check_diagonal(board)
-    check_diagonal(board) -1
-  elsif check_middle(board)
-    check_middle(board) -1
-  else
-    random_square(board) -1
-  end
-end
-
-def player_turn(board, player_symbol)
-  player_input = ''
-    loop do 
-      prompt "Select a square numbered 1-9."
-      player_input = gets.chomp.to_i - 1
-      return player_input if (0..9).include?(player_input)
-      prompt "The number needs to be between 0 - 9!"
-      puts ''
-    end
+def computer_turn(board, symbol)
+  winning_arr = []
+  #loop do
+    offensive_squares = find_potential_plays(board, symbol)
+    defensive_squares = find_potential_plays(board, change_symbol(symbol))
     
+
+    square = if offensive_squares
+      binding.pry
+               choose_which_play(offensive_squares, board, symbol)
+             elsif
+               choose_which_play(defensive_squares, board, symbol)
+             elsif
+              one_filled_square(board, symbol)
+             elsif
+              middle_square(board, symbol)
+             else
+              random_square(board, symbol)
+             end
+    
+    # square = 10
+    # squares_to_play.each do |arr|      
+    #   if arr[0] == 2
+    #     WINNING_LINES[arr[1]].each do |num|
+    #       square = if board[num] != symbol && square_free?(board, num)
+    #                   board[num]
+    #                 else
+    #                   next
+    #                 end
+    #     end
+    #   end
+    # end
+
+    
+    #p symbol
+    # return square if square != 10
+  #end
 end
+    # if line.count('X') == 2
+    #   line.select { |num| num != symbol }
+    # end
 
-def current_player_turn(board,current_player, player_symbol)
-  current_player == 'Player' ? player_turn(board, player_symbol) : computer_turn(board, player_symbol)
-end
+# check all the winning lines for an offensive move
+#
+# check all the winning lines for a defensive move
+# play the middle square
+# play square at random
 
-def winner?(board)
 
-  counter = 0
-  loop do 
-    return true if board[counter,3].count('X') == 3 || board[counter,3].count('O') == 3
-    break if counter > 7
-    counter += 3
-  end
+# def open_square(board, num, symbol)
+#   p num
+#   binding.pry
+#   return num if board[num] != symbol
+# end
 
-  counter = 0
-  loop do
-    return true if [board[counter], board[counter + 3], board[counter + 6]].count('X') == 3 || [board[counter], board[counter + 3], board[counter + 6]].count('O') == 3
-    break if counter > 3
-    counter += 1
-  end 
+# symbol = 'O'
+# board = ['X','X',3,4,5,6,7,8,9]
+# p computer_turn(board, symbol)
 
-  return true if [board[0], board[4], board[8]].count('X') == 3 || [board[0], board[4], board[8]].count('O') == 3
-  return true if [board[2], board[4], board[6]].count('X') == 3 || [board[2], board[4], board[6]].count('O') == 3
-end
 
-def board_full?(board)
-  board.count('X') + board.count('O') == 9
-end
-   
-system("clear")
-prompt "Welcome to Tic Tac Toe!"
-prompt "Choosing who goes first...."
-5.times do
-  sleep 0.25
-  print '.'
-end
-puts '.'
-current_player = pick_first_player
-prompt "#{current_player} goes first!"
+# 1. go through winning_lines and find out which lines have 2 of the computers symbols.
+# 2. find the winning_lines index and the index of the open square.
+# 3. computer should play the index of the 
 
-player_symbol = choose_symbol
-computer_symbol = set_computer_symbol(player_symbol)
-current_player_symbol = current_player == 'Player' ? player_symbol : computer_symbol
 
-board = initialize_board
-display_table(board)
-square = 10
-loop do
-  square = current_player_turn(board,current_player, current_player_symbol)
-  if square_free?(board, square)
-    place_piece(board, current_player, square, current_player_symbol)
-  else
-    prompt "That square is taken.  Please choose another."
-    square = gets.chomp.to_i - 1
-    square = current_player_turn(board,current_player, current_player_symbol)
-  end
-  break if board_full?(board)
-  break if winner?(board)
-  sleep 1
-  display_table(board)
-  prompt "#{current_player} choose to play square #{square + 1}."
-  current_player = switch_player(current_player)
-  current_player_symbol = current_player == 'Player' ? player_symbol : computer_symbol
-  sleep 1
-end
-display_table(board)
-
-puts ''
-board_full?(board) ? (prompt "Looks like it is a tie!") : (prompt "#{current_player} wins!")
 
 # check_last_square
-# board1 = ['X','O','X','O','X','O','X','O','X']
-# check_last_square(board1)
+# symbol = 'X'
+# board = ['X','O','X','O','X','O','X','O',9]
+# p computer_turn(board, symbol)
+#check_last_square(board1)
 
 # current_player = 'computer'
-# board = [0,1,2,3,4,5,'O','O',8]
+symbol = 'O'
+board = ['X','X',2,3,4,5,'O',7,8]
+#computer_turn(board, symbol)
+place_piece(board, computer_turn(board, symbol), symbol)
+p board
+
 # p computer_turn(board) == [0,1,2,3,4,5,'O','O','O']
 
 # # down test - should play square 8
 # current_player = 'computer'
-# board = [0,1,'O',3,4,'O',6,7,8]
+ board = [0,1,'O',3,4,'O',6,7,8]
+symbol = 'O'
+p computer_turn(board, symbol)
+place_piece(board, computer_turn(board, symbol), symbol)
+p board
 # p computer_turn(board) == [0,1,'O',3,4,'O',6,7,'O']
 
 # # diagonal test - should play square 6
 # current_player = 'computer'
-# board = [0,1,'O',3,'O',5,6,7,8]
+symbol = 'O'
+board = [0,1,'O',3,'O',5,6,7,8]
+p computer_turn(board, symbol)
+place_piece(board, computer_turn(board, symbol), symbol)
+p board
 # p computer_turn(board) == [0,1,'O',3,'O',5,'O',7,8]
 
 # # middle test - should play square 4
