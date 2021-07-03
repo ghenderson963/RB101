48f1bf96992c5d13f080462c1b4a216617bc08d0
Gary <ghenderson963@gmail.com>
2021-07-02 19:45:22 -0700

    added additional files

---
+++ lesson_6/twenty-one/scratch.rb
@@ -0,0 +1,20 @@
+# welcome player to 21
+# create a deck of cards
+# deal 2 cards to player
+# deal 2 cards to the dealer
+# total the cards
+# check for 21
+# display the players cards
+# display the dealers cards but only display the first one
+# Ask the player if they would like to hit or stay
+# if they hit 
+#   deal them a card
+#   total their cards
+#   display their total
+#   Ask if they would like to hit or stay
+# if they stay
+#   total their cards
+#   check if they've busted or have 21
+# dealers turn
+# 
+# 
