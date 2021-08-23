cf164b5783d2875a1ae4c82f331bc14c2cbc1b8b
Gary Henderson <ghenderson963@gmail.com>
2021-08-23 09:48:55 -0700

    working through easy5.rb

--- small_problems/easy5.rb
+++ small_problems/easy5.rb
@@ -152,3 +152,166 @@ end
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
 p swap('a') == 'a'
+
+# Given a string that consists of some words and an assortment of non-alphabetic characters,
+# write a method that returns a string with all of the non-alphabetic characters replaced by spaces.  
+# If one or more non-alphabetic characters occur in a row, you should only have one space in the result
+
+# break the string into substrings
+# iterate through the 
+
+def cleanup(phrase)
+  phrase.gsub(/[^a-zA-Z]+/, ' ')
+end
+
+p cleanup("---what's my +*& line?") == ' what s my line '
+
+# Write a method that takes a string with one or more space separated words and returns a hash
+# that shows the number of words of different sizes.
+# words consist of any string of characters that do not include a space.
+
+# create a hash to store the counts
+# break up the string into sub strings
+# iterate through the sub strings
+# count the number of letters in the word
+# place the number letters as the key
+# if the key already exists
+#   increment the value by 1
+# if the key doesn't exist
+#   put a 1 in the value
+
+def word_sizes(phrase)
+  counts = Hash.new(0)
+  phrase.split.each do |word|
+    counts[word.length] += 1
+  end
+  counts
+end
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('') == {}
+
+# Modify the word_sizes method from the previous exercise to exclude non-letters when determining word size.
+# example "it's" is 3, not 4
+
+def word_sizes(phrase)
+  counts = Hash.new(0)
+  phrase.split.each do |word|
+    counts[word.delete('^a-zA-Z').length] += 1
+  end
+  counts
+end
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+p word_sizes('') == {}
+
+# Write a method that takes an Array of integers between 0 and 19, and returns an
+# array of those integers sorted based on the English words for each number:
+
+# zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, 
+# thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
+
+# swap the elements in the array for their words
+# sort the new array alphabetically
+# swap the elements in the array back to their numbers
+# 
+# build a hash that converts numbers to words and the reverse
+# iterate through the array and convert to words
+# sort the new array using sort
+# iterate through the array and switch it back to integers
+require 'pry'
+
+DIGITS_TO_WORDS = { 
+  0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
+  6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten', 11 => 'eleven',
+  12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
+  17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen'
+}
+
+def alphabetic_number_sort(arr)
+  arr.sort_by { |num| DIGITS_TO_WORDS[num] } 
+end
+
+puts ''
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
+
+# Write a method that takes a string argument and returns a new string that contains the value
+# of the original string with all consecutive duplicate characters collapsed into a single 
+# character.
+# don't use squeeze
+
+# convert the string into and array of sub-strings
+# iterate through the strings
+# if the string matches the string before it
+#    delete the string
+# if not keep it
+# convert the new array back into a string.
+ 
+def crunch(phrase)
+  last_char = ''
+  phrase.chars.map do |char|
+    next if char == last_char
+    last_char = char
+  end.join
+end
+
+
+p crunch('ddaaiillyy ddoouubbllee') #== 'daily double'
+p crunch('4444abcabccba') == '4abcabcba'
+p crunch('ggggggggggggggg') == 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
+puts ''
+
+# Write a method that will take a short line of text, and print it within a box.
+# the input will always fit in the terminal window.
+
+# find the length of the phrase
+# print a '+' followed by the length of the phrase + 2
+# print a '|' followed by the length of the phrase + 2 and then a '+'
+# print a '|' then a space then the phrase then a space then a '|'
+# print the 2st line again
+# print the 1st line again
+
+def print_border(phrase_length)
+  print '+'
+  phrase_length.times { print '-' }
+  puts '+'
+end
+
+def print_middle(phrase_length)
+  print '|'
+  phrase_length.times { print ' '}
+  puts '|'
+end
+
+def print_in_box(phrase)
+  phrase_length = phrase.length + 2
+  print_border(phrase_length)
+  print_middle(phrase_length)
+  puts "| #{phrase} |"
+  print_middle(phrase_length)
+  print_border(phrase_length)
+end
+
+
+print_in_box('To boldly go where no one has gone before.')
+# +--------------------------------------------+
+# |                                            |
+# | To boldly go where no one has gone before. |
+# |                                            |
+# +--------------------------------------------+
+
+print_in_box('')
+# +--+
+# |  |
+# |  |
+# |  |
+# +--+
