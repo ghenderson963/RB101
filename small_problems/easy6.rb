55f408f8bc8f583044646ebd537244878d558d08
Gary Henderson <ghenderson963@gmail.com>
2021-09-04 15:06:08 -0700

    working on easy6.rb

--- small_problems/easy6.rb
+++ small_problems/easy6.rb
@@ -14,25 +14,25 @@ DEGREE = "\xC2\xB0"
 # repeat
 
 
-def get_number(flt)
-  arr = []
-  if [0, 360].include?(flt)
-    return [0,0,0]
-  else
-    3.times do 
-      first, second = flt.to_s.split('.')
-      second = '.'.concat(second).to_f
-      arr << first.to_f
-      flt = second * 60
-    end
-  end
-  arr
-end
+# def get_number(flt)
+#   arr = []
+#   if [0, 360].include?(flt)
+#     return [0,0,0]
+#   else
+#     3.times do 
+#       first, second = flt.to_s.split('.')
+#       second = '.'.concat(second).to_f
+#       arr << first.to_f
+#       flt = second * 60
+#     end
+#   end
+#   arr
+# end
 
-def dms(flt)
-  arr = get_number(flt)
-  format("%.2d",arr[0]) + "#{DEGREE}" + format("%.2d",arr[1]) + %Q(') + format("%.2d",arr[2]) + %(")
-end
+# def dms(flt)
+#   arr = get_number(flt)
+#   format("%.2d",arr[0]) + "#{DEGREE}" + format("%.2d",arr[1]) + %Q(') + format("%.2d",arr[2]) + %(")
+# end
 
   # take the right half and convert to a float and multiple by 60
   # split the new number and store the left half
@@ -51,14 +51,36 @@ end
 
 # working on a better method that is easier to read and understand
 # the above fails if there is no decimal
+# find the total seconds 
+# 360 minutes in a degree
+# 60 minutes in a second 
+# The first part of the number becomes the degrees
+# multiply the second part by 60 
+# the first part becomes the minutes
+# multiple the second part by 60 to get the seconds
+# so
+# 
+SECONDS_PER_MINUTE = 60
+def dms(flt)
+  degrees, minutes = flt.divmod(1)
+  minutes *= SECONDS_PER_MINUTE
+  _,seconds = minutes.divmod(1)
+  seconds *= SECONDS_PER_MINUTE
 
 
-#puts dms(30) #== %(30°00'00")  # 30 = 30 00 00
-puts dms(76.73) #== %(76°43'48") #76 degrees + 0.73 degrees * 60 = 43.8 = 76 degrees 43.8 minutes x 60 = 48 == 76degrees 43 minutes 48 seconds 
-puts dms(254.6) #== %(254°36'00")
-puts dms(93.034773) #== %(93°02'05")
-puts dms(0) #== %(0°00'00")
-puts dms(360) #== %(360°00'00") || dms(360) == %(0°00'00")
+  p degrees
+  p minutes
+  p seconds
+
+end
+
+
+ dms(30) #== %(30°00'00")  # 30 = 30 00 00
+ dms(76.73) #== %(76°43'48") #76 degrees + 0.73 degrees * 60 = 43.8 = 76 degrees 43.8 minutes x 60 = 48 == 76degrees 43 minutes 48 seconds 
+ dms(254.6) #== %(254°36'00")
+ dms(93.034773) #== %(93°02'05")
+ dms(0) #== %(0°00'00")
+ dms(360) #== %(360°00'00") || dms(360) == %(0°00'00")
 
 dms(400) == %(400°00'00")
 dms(-40) == %(-40°00'00")
