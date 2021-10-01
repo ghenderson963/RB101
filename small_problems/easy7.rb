073456b9f6ee653b8515cfb86820e689430995f0
Gary Henderson <ghenderson963@gmail.com>
2021-09-30 20:50:39 -0700

    working on easy7.rb finished penultimatev2.rb

--- small_problems/easy7.rb
+++ small_problems/easy7.rb
@@ -122,7 +122,7 @@ p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
 # convert the array into a string
 #   !letter.match?(/[a-zA-Z]/)
 
-def staggered_case(phrase)
+def staggered_casev1(phrase)
   count = -1
   phrase.chars.map do |letter|
     count += 1
@@ -137,9 +137,9 @@ def staggered_case(phrase)
   
 end
 
-p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-p staggered_case('ALL_CAPS') == 'AlL_CaPs'
-p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_casev1('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_casev1('ALL_CAPS') == 'AlL_CaPs'
+p staggered_casev1('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
 
 # Modify the method above so it ignores non-alphabetic characters when determining whither
 # it should uppercase or lowercase each letter.
@@ -155,7 +155,7 @@ p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
 #         place the substring with no changes
 
 
-def staggered_casev1(phrase)
+def staggered_casev2(phrase)
   upcase = true
   result = ''
   phrase.chars.each do |char|
@@ -177,4 +177,172 @@ p staggered_casev1('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 p staggered_casev1('ALL CAPS') == 'AlL cApS'
 p staggered_casev1('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
 
-#
+# Modify the above method so that so the caller can determine whether non-alphabetic characters should be counted
+# when determining the upper/lowercase state.
+
+# initialize variable for the result string
+# initualize a case variable and set it to true
+# initialize a variable to track whether or not we count non-alpha
+# set the above variable based on count or nocount set it to true or false
+# if we count non-alpha
+#   if upcase 
+#     change to upcase
+#   if upcase == false
+#     change to downcase
+# if we don't count non-alpha
+#   if upcase
+#     change to upcase
+#   if downcase
+#     change to downcase
+#   else
+#     add the char
+# 
+
+
+
+def staggered_casev3(phrase, count_alpha)
+  if count_alpha
+    staggered_casev1(phrase)
+  else
+    staggered_casev2(phrase)
+  end
+
+end
+
+p staggered_casev3('I Love Launch School!', false) == 'I lOvE lAuNcH sChOoL!'
+p staggered_casev3('ALL CAPS', false) == 'AlL cApS'
+p staggered_casev3('ignore 77 the 444 numbers', false) == 'IgNoRe 77 ThE 444 nUmBeRs'
+
+p staggered_casev3('I Love Launch School!', true) == 'I LoVe lAuNcH ScHoOl!'
+p staggered_casev3('ALL_CAPS', true) == 'AlL_CaPs'
+p staggered_casev3('ignore 77 the 444 numbers', true) == 'IgNoRe 77 ThE 444 NuMbErS'
+
+# write a method that takes an array of integers as input, multiplies all the numbers together, divides the result
+# by the number of entries in the Array, and then prints the result rounded to 3 decimal places.  Assume the array
+# is non-empty.
+
+# initialize a variable to hold the result
+# iterate through the array
+#   during each iteration
+#    multipley the number with the result
+# take the result and divide it by the number of elements in the array
+# round the result to 3 decimal places
+
+def show_multiplicative_average(arr)
+  result = 1.0
+  arr.each { |num| result *= num }
+  result = (result / arr.length)
+  format('%.3f', result)
+end
+
+
+
+p show_multiplicative_average([3, 5])                # => The result is 7.500
+p show_multiplicative_average([6])                   # => The result is 6.000
+p show_multiplicative_average([2, 5, 7, 11, 13, 17]) # => The result is 28361.667 
+
+# Write a method that takes 2 array arguments in which each array contains a list of numbers and returns a new array
+# that contains the product of each pair of numbers from the arguments that have the same index.  
+# Assume that the arguments contain the same number of elements.
+
+# brut force
+# initialize an array for the results
+# initialize a counter to zero
+# create a loop and iterate through the arrays
+#   in each iteration multiply the similar indexes
+#   pass them into the results array
+# return the results array
+
+def multiply_list(arr1, arr2)
+  results = []
+  counter = 0
+  loop do 
+    break if counter >= arr1.length
+    results << arr1[counter] * arr2[counter]
+    counter += 1
+  end
+  results
+end
+
+def multiply_listv2(arr1, arr2)
+  results = []
+  arr1.each_with_index { |num, index| results << num * arr2[index] }
+  results
+end
+
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+p multiply_listv2([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+# Write a method that takes two Array arguments in which each array contains a list of numbers, 
+# and returns a new array that contains the product of every pair of numbers that can be formed 
+# between the elements of the two arrays.
+# sort the results by increasing value
+# assument that neither argument is an empty array.
+
+# intialize an array for the results
+# create 2 loops 
+#   outer loop 
+#     iterate through arr1
+#     inner loop
+#       in each iteration multiply arr1 element with every element in the other array.
+#       place the result of each in the results array
+# sort the results array
+# return the results array
+#  
+
+def multiply_all_pairs(arr1, arr2)
+  results = []
+  arr1.each do |num1|
+    arr2.each do |num2|
+      results << num1 * num2
+    end
+  end
+  results.sort 
+end
+
+p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+
+# Write a method that returns the next to last word in the string passed to it as an argument.
+# words are any sequence of non-blank characters
+# assume that the input String will always contain at least 2 words
+
+# use split to break on the words
+# use [] to access the second to last sub-string
+
+def penultimate(str)
+  str.split[-2]
+end
+
+p penultimate('last word') == 'last'
+p penultimate('Launch School is great!') == 'is'
+
+# write a method that returieves the middle word of a phrase.
+# What edge cases need to be considered?  
+# 1. empty string - should return empty string 
+# 2. odd string - should warn and then return the left hand middle phrase
+
+# split the string into an array
+# find the length of the array
+# if the length is even
+#   chose index divide the length by 2 and use - the length - result - 1 divid the length of the array by 2
+#   result - length - 1
+# if the length is odd
+#  divide the length by 2 and multiply by -1
+# access the string by the above index
+
+def penultimatev2(str)
+  return '' if str.empty?
+  phrase = str.split
+  size = phrase.length 
+  if size.even?
+    phrase[(size / 2) - size - 1]
+  else
+    phrase[-size / 2]
+  end
+end
+
+
+p penultimatev2('') == ''
+p penultimatev2('I love hot donuts') == 'love'
+p penultimatev2('I love hotdogs and donuts') == 'hotdogs'
