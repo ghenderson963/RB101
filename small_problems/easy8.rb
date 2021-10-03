ec9094677c21218ed978258224d297ffb270187a
Gary Henderson <ghenderson963@gmail.com>
2021-10-02 17:28:32 -0700

    Starting easy8.rb

---
+++ small_problems/easy8.rb
@@ -0,0 +1,10 @@
+# Write a method that takes an array of numbers and then returns the sum of the sums of each of
+# each leading subsequence for that Array.  You may assume that the Array always contains at least 
+# one number
+
+
+
+sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+sum_of_sums([4]) == 4
+sum_of_sums([1, 2, 3, 4, 5]) == 35
