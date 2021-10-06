eb6ec9176bfd9659f6a6465d15549a583acd5f4b
Gary Henderson <ghenderson963@gmail.com>
2021-10-05 20:58:14 -0700

    working on easy8.rb

--- small_problems/easy8.rb
+++ small_problems/easy8.rb
@@ -40,21 +40,108 @@ p sum_of_sumsv2([4]) == 4
 p sum_of_sumsv2([1, 2, 3, 4, 5]) == 35
 
 # Write a simple program that asks for a noun, verb,adjective, and adverb.  Then writes a sentence using them.
+# 
+# print 'Enter a noun: '
+# noun = 'dog'
+# print 'Enter a verb: '
+# verb = 'running'
+# print 'Enter an adjective: '
+# adjective = 'green'
+# print 'Enter an adverb: '
+# adverb = 'quickly'
+# 
+# 
+# 
+# puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+# puts
+# 
+# parts_of_speach % {
+  # noun: noun,
+  # verb: verb,
+  # adjective: adjective,
+  # adverb: adverb
+# }
+# 
+# File.foreach('./sentences.txt') do |line|
+  # line.gsub!(/sentence_[1-9]+ = /,'')
+  # puts "%s #{line}"
+# end
 
-print 'Enter a noun: '
-noun = gets.chomp 
-print 'Enter a verb: '
-verb = gets.chomp
-print 'Enter an adjective: '
-adjective = gets.chomp
-print 'Enter an adverb: '
-adverb = gets.chomp
 
+# Write a method that returns a list of all substrings of a string that start at the beginning of the
+# original string.
+# The return value should be arranged in order from shortest to longest substring.
 
+# initialize a integer as a counter
+# create a loop
+# break when the counter reaches the size of the arr
+#   in each iteration 
+#   add to the arr a slice from zero to the n index
+# 
 
-puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
-puts 
-
-File.foreach('./sentences.txt') do |line|
-  line.split.map do |
+def leading_substrings(str)
+  result = []
+  counter = 0
+  loop do
+    break if counter >= (str.length)
+    result << str.slice(0..counter)
+    counter += 1
+  end
+  result 
 end
+
+def leading_substringsv2(str)
+  results = []
+  0.upto(str.length - 1) do |indx|
+    results << str.slice(0..indx)
+  end
+  results
+end
+
+def leading_substringsv3(str)
+  results = []
+  str.chars.each_with_index do |_ , indx|
+    results << str[0..indx]
+  end
+  results.to_a
+
+end
+
+p leading_substrings('abc') == ['a', 'ab', 'abc']
+p leading_substrings('a') == ['a']
+p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+p leading_substringsv2('abc') #== ['a', 'ab', 'abc']
+p leading_substringsv2('a') == ['a']
+p leading_substringsv2('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+p leading_substringsv3('abc') #== ['a', 'ab', 'abc']
+p leading_substringsv3('a') == ['a']
+p leading_substringsv3('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+
+# Write a method that returns a list of all substrings of a string.  The returned list 
+# should be ordered by where in the string the substring begins. 
+
+def substrings(str)
+  results = []
+  counter = 0
+  loop do
+    break if counter >= str.length
+    
+    counter.upto(str.length - 1) do |indx|
+      results << str.slice(counter..indx)
+    end
+    counter += 1
+  end
+    results.to_a
+end
+
+
+ substrings('abcde') == [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
