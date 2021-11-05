f01a301f119c1b6f1ab9523595bce5fa39b9ed7c
Gary Henderson <ghenderson963@gmail.com>
2021-11-04 22:45:19 -0700

    finished manilang

--- small_problems/medium1.rb
+++ small_problems/medium1.rb
@@ -233,3 +233,103 @@ diamond(1)
 diamond(3)
 diamond(9)
 
+# Write a method that implements a miniature stack-and-register-based programming language that has the
+# following commands:
+# n Place a value n in the "register". Do not modify the stack.
+# PUSH Push the register value on to the stack. Leave the value in the register.
+# ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
+# SUB Pops a value from the stack and subtracts it from the register value, storing the result in the register.
+# MULT Pops a value from the stack and multiplies it by the register value, storing the result in the register.
+# DIV Pops a value from the stack and divides it into the register value, storing the integer result in the register.
+# MOD Pops a value from the stack and divides it into the register value, storing the integer remainder of the division in the register.
+# POP Remove the topmost item from the stack and place in register
+# PRINT Print the register value
+
+# use and array and push and pop methods
+# stack = array
+# register = probably a variable int
+
+# 1. divide the argument string on spaces
+# 2. iterate through the sub-string arguments
+# 3.   check if the arguement is a int
+#        if the argument is a number 
+#           push it onto the register
+#        else
+#           perform the operation
+#
+
+def compute(stack, register, command)
+
+  case command
+  when 'PUSH'
+     stack.push(register)
+     register
+  when 'ADD'
+    register = stack.pop + register
+  when 'SUB'
+    register = register - stack.pop
+  when 'MULT'
+    register = stack.pop * register
+  when 'DIV'
+    register = register / stack.pop
+  when 'MOD'
+    _, register = register.divmod(stack.pop)
+    register
+  when 'POP'
+    register = stack.pop
+  when 'PRINT'
+    p register
+  else
+    puts 'ERROR: #{command} command.'
+  end
+
+end
+
+
+def minilang(input)
+  commands = input.split
+  stack = []
+  register = 0
+ 
+  commands.each do |command|
+    if command.to_i.to_s == command
+      register = command.to_i
+    else
+      register = compute(stack, register, command)
+    end
+  end
+
+end
+
+# minilang('3 4 ADD')
+minilang('PRINT')
+# 0
+
+minilang('5 PUSH 3 MULT PRINT')
+# 15
+
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# 5
+# 3
+# 8
+
+minilang('5 PUSH POP PRINT')
+# 5
+
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# 5
+# 10
+# 4
+# 7
+
+minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# 6
+
+minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# 12
+
+minilang('-3 PUSH 5 SUB PRINT')
+# 8
+
+minilang('6 PUSH')
+# (nothing printed; no PRINT commands)
